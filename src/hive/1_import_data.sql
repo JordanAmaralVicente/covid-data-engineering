a8d4820c33ac48ceec75eeb6c49078d971552849
JordanAmaralVicente <jordanitalovicente@gmail.com>
2022-04-21 18:28:29 -0300

    feat: create sql file to import data from hdfs on hive

---
+++ src/hive/1_import_data.sql
@@ -0,0 +1,30 @@
+-- Import data from hdfs to an external table
+CREATE DATABASE IF NOT EXISTS covid;
+
+USE covid;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS e_casos_covid(
+    regiao STRING,
+    uf CHAR(2),
+    municipio STRING,
+    cod_uf TINYINT,
+    cod_mun INT,
+    cod_regiao_saude INT,
+    nome_regiao_saude STRING,
+    data DATE,
+    semana_epi TINYINT,
+    populacao INT,
+    casos_acumulado INT,
+    casos_novos INT,
+    obitos_acumulado INT,
+    obitos_novos INT,
+    recuperados_novos INT,
+    acompanhamento_novos INT,
+    interior_metropolitana INT
+)
+COMMENT 'External Table With Covid Numbers'
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ';' 
+STORED AS TEXTFILE 
+LOCATION '/user/root/data/'
+tblproperties('skip.header.line.count'='1');
